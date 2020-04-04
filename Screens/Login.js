40716ce020f264ada2dfb3bfa8079b4d4a31618d
islamblmrbt <belmerabetislam@gmail.com>
2020-04-04 12:55:11 +0100

    Code Arrangement

--- Screens/Login.js
+++ Screens/Login.js
@@ -8,7 +8,7 @@ export default function Login(props) {
   const [rememberMe, setRememberMe] = useState(false);
 
   const userNameHandler = userName => {
-    setUserName(userName);
+    setUserName(userName);console.log('')
   };
 
   const passwordHandler = password => {
