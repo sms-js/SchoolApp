1cb3813d45d98d620d76dd68c502078a63df58a3
islamblmrbt <belmerabetislam@gmail.com>
2020-04-18 12:44:55 +0100

    profile pic

--- Navigation/navigatorHeader.js
+++ Navigation/navigatorHeader.js
@@ -1,16 +1,30 @@
 import React from 'react';
-import {Text, ScrollView, View, StyleSheet} from 'react-native';
+import {Text, ScrollView, View, StyleSheet, Image} from 'react-native';
 import {DrawerNavigatorItems} from 'react-navigation-drawer';
 import {useAuth} from '../context/Authentication';
+import {server} from '../utils/config';
 
 export default function NavigatorHeader(props) {
   const {user} = useAuth();
-
+  let profilePictureUri = '';
+  if (user['photo'] === '') {
+    profilePictureUri =
+      'https://cdn.clipart.email/23d223de4bd3900b3c41fcd52b5ef3b1_avatar-circle-general-human-person-user-icon_512-512.png';
+    //server + 'uploads/profile/user.png';
+  } else {
+    profilePictureUri = server + 'uploads/profile/' + user['photo'];
+  }
   return (
     <ScrollView>
       <View style={styles.view}>
-        <View style={styles.profile} />
+        <Image
+          style={styles.profile}
+          source={{
+            uri: profilePictureUri, //server + '/uploads/profile/' + user['photo'],
+          }}
+        />
         <Text style={styles.text}>{user['userName']}</Text>
+        <Text style={{fontSize: 17.5, margin: 5}}>{user['fullName']}</Text>
       </View>
       <View>
         <DrawerNavigatorItems {...props} />
@@ -28,11 +42,11 @@ const styles = StyleSheet.create({
     margin: 5,
   },
   profile: {
-    width: 80,
-    height: 80,
-    borderRadius: 40,
-    backgroundColor: 'black',
+    width: 90,
+    height: 90,
+    borderRadius: 100,
     margin: 5,
+    resizeMode: 'center',
   },
 });
 
--- utils/config.js
+++ utils/config.js
@@ -1,1 +1,2 @@
 export const BASE_URL = 'http://hou11am.herokuapp.com/app/api/';
+export const server = 'http://hou11am.herokuapp.com/';
