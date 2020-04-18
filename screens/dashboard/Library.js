0babd293113fb3e3db18a9572c3a9a322cbacbdc
islamblmrbt <belmerabetislam@gmail.com>
2020-04-18 22:12:58 +0100

    latest

--- screens/dashboard/Library.js
+++ screens/dashboard/Library.js
@@ -1,9 +1,32 @@
-import React from 'react';
-import {Text, ScrollView, View, Button} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  ScrollView,
+  View,
+  Button,
+  TouchableOpacity,
+  FlatList,
+  TextInput,
+  StyleSheet,
+} from 'react-native';
 import {Header, Left, Icon} from 'native-base';
 import {fetchBooks, searchBooks} from './api/fetchBooks';
+import {Dropdown} from 'react-native-material-dropdown';
 
 export default function Library(props) {
+  const [books, setBooks] = useState([{}]);
+  const [searchedBook, setSearchedBook] = useState('');
+  const [searchCriteria, setSearchCriteria] = useState('');
+  const [filterCriteria, setFilterCriteria] = useState('');
+  const searchedBookHandler = (value) => {
+    setSearchedBook(value);
+  };
+  const searchCriteriaHandler = (criteria) => {
+    setSearchCriteria(criteria);
+  };
+  const filterCriteriaHandler = (criteria) => {
+    setFilterCriteria(criteria);
+  };
   return (
     <View>
       <Header
@@ -32,31 +55,73 @@ export default function Library(props) {
       </Header>
       <ScrollView style={{margin: 20}}>
         <Text />
-        <View>
-          <Text style={{alignSelf: 'center'}}>Library</Text>
-          <Text />
-          <Button
-            title="Show all books"
-            onPress={() => {
-              fetchBooks();
-            }}
-          />
-          <Text />
-          <Button
-            title="Show Book 1"
-            onPress={() => {
-              searchBooks('searchbytitle', 'Book 1');
-            }}
-          />
-          <Text />
-          <Button
-            title="Login Screen"
-            onPress={() => {
-              props.navigation.navigate('Login');
+
+        <TextInput
+          style={styles.searchInput}
+          placeholder="Search for"
+          onChangeText={searchedBookHandler}
+        />
+        <View style={styles.drop}>
+          <Dropdown
+            label="Search by"
+            data={[
+              {label: 'Book Title', value: 1},
+              {label: 'Author Name', value: 2},
+            ]}
+            onChangeText={(value) => {
+              searchCriteriaHandler(value);
             }}
           />
         </View>
+        <Text />
+        <TouchableOpacity
+          style={styles.searchOpacity}
+          onPress={() => {
+            //searchBooks('searchbytitle', 'Book 1');
+            alert('sb: ' + searchedBook + '\nsc: ' + searchCriteria);
+          }}>
+          <Text style={{marginTop: 10, alignSelf: 'center'}}>Search</Text>
+        </TouchableOpacity>
+        <View style={styles.filterView}>
+          <Text style={{width: '30%', fontSize: 17.5, paddingTop: 17.5}}>
+            Show
+          </Text>
+          <View style={{width: '70%'}}>
+            <Dropdown
+              label="Books by"
+              data={[
+                {label: 'All Books', value: 1},
+                {label: 'Traditional Books', value: 2},
+                {label: 'Electronic Books', value: 3},
+              ]}
+              onChangeText={(value) => {
+                filterCriteriaHandler(value);
+              }}
+            />
+          </View>
+        </View>
+        <Text />
+
+        <FlatList data={books} renderItem={({item}) => {}} />
       </ScrollView>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  filterView: {alignItems: 'center', flexDirection: 'row'},
+  searchInput: {
+    padding: 5,
+    borderColor: 'lightblue',
+    borderWidth: 1,
+  },
+  drop: {flex: 1, justifyContent: 'center', height: 70},
+  searchOpacity: {
+    height: 40,
+    width: '80%',
+    backgroundColor: 'lightblue',
+    alignSelf: 'center',
+    borderRadius: 30,
+    marginBottom: 5,
+  },
+});
