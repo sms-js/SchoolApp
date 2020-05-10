714e2c4179110da9ab283dcf73ef4878773892bf
islamblmrbt <belmerabetislam@gmail.com>
2020-05-10 23:14:51 +0100

    latest

--- screens/dashboard/OnlineExams/TakeOnlineExam.js
+++ screens/dashboard/OnlineExams/TakeOnlineExam.js
@@ -8,8 +8,15 @@ import {
   Button,
 } from 'react-native';
 import {Header, Left, Icon} from 'native-base';
+import RadioForm, {
+  RadioButton,
+  RadioButtonInput,
+  RadioButtonLabel,
+} from 'react-native-simple-radio-button';
 
 export default function TakeOnlineExam(props) {
+  const [a, setA] = useState();
+
   return (
     <View>
       <Header
@@ -37,12 +44,44 @@ export default function TakeOnlineExam(props) {
         <Text style={{width: '15%'}} />
       </Header>
       <ScrollView style={{margin: 20, marginBottom: 60}}>
-        <Text>Take Online Exam</Text>
+        <Text>{props.navigation.state.params.exam.subjectTitle}</Text>
+        <Text>{props.navigation.state.params.exam.examTitle}</Text>
+        <Text>{props.navigation.state.params.exam.examDescription}</Text>
+        <Text>{props.navigation.state.params.exam.examDate}</Text>
+        <Text>{props.navigation.state.params.exam.ExamEndDate}</Text>
+        <FlatList
+          data={props.navigation.state.params.exam.examQuestion}
+          renderItem={({item}) => (
+            <View style={styles.question}>
+              <Text style={{alignSelf: 'center'}}>
+                question{' '}
+                {props.navigation.state.params.exam.examQuestion.indexOf(item) +
+                  1}
+              </Text>
+              <Text>{item.questionText}</Text>
+              <Text />
+              <RadioForm
+                radio_props={[
+                  {label: item.answer1, value: 1},
+                  {label: item.answer2, value: 2},
+                  {label: item.answer3, value: 3},
+                  {label: item.answer4, value: 4},
+                  {label: 'no answer', value: 5},
+                ]}
+                initial={5}
+                onPress={(value) => {
+                  setA(value);
+                }}
+              />
+            </View>
+          )}
+        />
         <Text />
         <Button
           title="show exam"
           onPress={() => {
-            console.log(props.navigation.state.params.exam);
+            alert(a);
+            // console.log(props.navigation.state.params.exam);
           }}
         />
         <Text />
@@ -56,3 +95,12 @@ export default function TakeOnlineExam(props) {
     </View>
   );
 }
+const styles = StyleSheet.create({
+  question: {
+    borderColor: 'lightblue',
+    borderWidth: 3,
+    borderRadius: 30,
+    margin: 5,
+    padding: 10,
+  },
+});
